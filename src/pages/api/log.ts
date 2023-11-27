85e0408184bd46466ad7a46b85cc8333d866fcee
Niceygy <niceygy@niceygylive.xyz>
2023-11-27 17:20:39 +0000

    add comments!!

--- src/pages/api/log.ts
+++ src/pages/api/log.ts
@@ -1,3 +1,11 @@
+
+/**
+ * The default log function for the API.
+ * Will only log if the environment variable DEV is set to true.
+ * @param message The message to log
+ * @param funcName The name of the function calling this
+ * @returns Nothing
+ */
 export default function log(message: string, funcName: string) {
     if (process.env.DEV === 'true') {
   console.log(`[API] ${funcName}: ${message}`)
--- src/pages/api/users/db.ts
+++ src/pages/api/users/db.ts
@@ -4,7 +4,12 @@ import log from "../log";
 
 const auth = getAuth(app);
 
-
+/**
+ * Wrapper for firebases auth function.
+ * @param email The inputted email
+ * @param password The inputted password
+ * @returns The user object if successful, Error if not.
+ */
 export function authUser(email: string, password: string) {
   log(`Authenticating user with email ${email}`, 'authUser')
   signInWithEmailAndPassword(auth, email, password)
@@ -20,6 +25,12 @@ export function authUser(email: string, password: string) {
     });
 }
 
+/**
+ * The function to create a new user.
+ * @param email The inputted email
+ * @param password The inputted password
+ * @returns The user object if successful, Error if not.
+ */
 export function newUser(email: string, password: string) {
   log(`Creating user with email ${email}`, 'newUser')
   createUserWithEmailAndPassword(auth, email, password)
