034ac61790f074d7a5bf8556759e9115b030db7a
Niceygy <niceygy@niceygylive.xyz>
2023-11-27 16:54:17 +0000

    recipes DB funcs pt.1

--- src/pages/api/food/db.ts
+++ src/pages/api/food/db.ts
@@ -1,6 +1,22 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 import log from "../log";
+
+export type Recipe = {
+    ID: string
+    name: string
+    ingredients: string[]
+    instructions: string[]
+    tags: string[]
+}
+
+
 
 export async function getRecipe(ID: string) {
     log(`Getting recipe with ID ${ID}`, 'getRecipe')
-
-}
+    const querySnapshot = await getDocs(collection(db, 'recipes'));
+    const recipes = querySnapshot.docs.map(doc => doc.data() as Recipe);
+    const recipe = recipes.find(recipe => recipe.ID === ID);
+    log(`Got recipe with ID ${ID}`, 'getRecipe')
+    return recipe; 
+};
