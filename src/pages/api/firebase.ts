d70d1592e1cbd52894d9e9ee26e25c50e292b7af
Niceygy <niceygy@niceygylive.xyz>
2023-12-09 14:02:08 +0000

    add cloud detection

--- src/pages/api/firebase.ts
+++ src/pages/api/firebase.ts
@@ -3,7 +3,22 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import fireFile from "../../../firebase.json";
 
-const firebaseConfig = {
+let firebaseConfig;
+
+if (process.env.NODE_ENV === 'production') {
+  // In a production environment, use environment variables
+  firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  };
+} else {
+  // In a local environment, use the firebase.json file
+  firebaseConfig = {
     apiKey: fireFile.apiKey,
     authDomain: fireFile.authDomain,
     projectId: fireFile.projectId,
@@ -11,7 +26,8 @@ const firebaseConfig = {
     messagingSenderId: fireFile.messagingSenderId,
     appId: fireFile.appId,
     measurementId: fireFile.measurementId
-};
+  };
+}
 
 /**
  * The firebase app object. 
